10aedde20840731e0022e2e337088e494b423331
Denis <seltix9381@gmail.com>
2024-04-06 17:16:56 +0300

    filter_operations

--- src/main.py
+++ src/main.py
@@ -4,3 +4,11 @@ import json
 def open_json_file(file_path):
     with open(file_path, encoding='utf-8') as f:
         return json.load(f)
+
+
+def filter_operations(data):
+    filtered_list = []
+    for operation in data:
+        if operation.get('state') == 'EXECUTED':
+            filtered_list.append(operation)
+    return filtered_list

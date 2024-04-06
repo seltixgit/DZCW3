c739ac0fdf3ab83bfff0fd26b46b766c900ff8f0
Denis <seltix9381@gmail.com>
2024-04-06 17:18:41 +0300

    mask_operation_from and mask_operation_to

--- src/main.py
+++ src/main.py
@@ -17,3 +17,27 @@ def filter_operations(data):
 def sort_operations(operations_data: list[dict]) -> list[dict]:
     sorted_list = sorted(operations_data, key=lambda x: x['date'], reverse=True)
     return sorted_list
+
+
+def mask_operation_from(operation):
+    operation_from = operation.get('from')
+
+    if operation_from:
+        parts = operation_from.split(' ')
+        numbers = parts[-1]
+        if len(numbers) == 16:
+            masked_numbers = f"{numbers[:4]} {numbers[4:6]}** **** {numbers[-4:]}"
+            return f"{" ".join(parts[:-1])} {masked_numbers}"
+        else:
+            return f'Счёт **{numbers[-4:]}'
+
+
+def mask_operation_to(operation):
+    operation_to = operation.get('to')
+
+    if operation_to:
+        parts = operation_to.split(' ')
+        numbers = parts[-1]
+        if len(numbers) == 20:
+            return f'Счёт **{numbers[-4:]}'
+
